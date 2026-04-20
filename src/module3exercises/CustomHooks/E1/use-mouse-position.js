9e8cc452882bb689ca1dd4e5b0ef7752ec89daef
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2026-04-20 14:39:29 +1200

    Module 3: CompletedExercise 7 part 1

---
+++ src/module3exercises/CustomHooks/E1/use-mouse-position.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function useMousePosition() {
+  // TODO
+  const [mousePosition, setMousePosition] = React.useState({
+    x: 0,
+    y: 0,
+  });
+  
+  React.useEffect(() => {
+    function handleMouseMove(event) {
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return mousePosition;
+}
+
+export default useMousePosition;
+
+// import React from 'react';
+
+// import useMousePosition from './hooks/use-mouse-position.js';
+
+// // TODO: Pull the mouse-position logic into
+// // the use-mouse-position.js file!
+
+// function App() {
+//   const mousePosition = useMousePosition();
+
+//   return (
+//     <div className="wrapper">
+//       <p>
+//         {mousePosition.x} / {mousePosition.y}
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default App;
