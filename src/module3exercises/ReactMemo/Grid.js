dacc07c4fa4efe644f523563de4c3cc58fae098a
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2026-04-21 14:36:54 +1200

    Module 3: Completed Exercise 9 part 1

---
+++ src/module3exercises/ReactMemo/Grid.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { range } from './utils';
+
+function Grid({ numRows, numCols }) {
+  console.info('Grid render');
+  
+  return (
+    <div className="grid-wrapper">
+      {range(numRows).map((rowIndex) => (
+        <div key={rowIndex} className="row">
+          {range(numCols).map((colIndex) => (
+            <div key={colIndex} className="cell" />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const PureGrid = React.memo(Grid);
+
+export default PureGrid;
