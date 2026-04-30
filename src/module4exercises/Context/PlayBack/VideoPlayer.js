5e5158e48d2c5fe18331c96be6d45c9ef9d8152d
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2026-05-01 10:28:50 +1200

    Module 4: Completed Exercise 6 part 1

---
+++ src/module4exercises/Context/PlayBack/VideoPlayer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { PlaybackRateContext } from './App';
+
+function VideoPlayer({
+  src,
+  caption
+}) {
+  const playbackRateSelectId = React.useId();
+  const {playbackRate, setPlaybackRate} = React.useContext(PlaybackRateContext);
+
+  const videoRef = React.useRef();
+
+  React.useEffect(() => {
+    videoRef.current.playbackRate = playbackRate;
+  }, [playbackRate]);
+
+  return (
+    <div className="video-player">
+      <figure>
+        <video ref={videoRef} controls src={src} />
+        <figcaption>{caption}</figcaption>
+      </figure>
+
+      <div className="actions">
+        <label htmlFor={playbackRateSelectId}>
+          Select playback speed:
+        </label>
+        <select
+          id={playbackRateSelectId}
+          value={playbackRate}
+          onChange={(event) => {
+            setPlaybackRate(event.target.value);
+          }}
+        >
+          <option value="0.5">0.5</option>
+          <option value="1">1</option>
+          <option value="1.25">1.25</option>
+          <option value="1.5">1.5</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default VideoPlayer;
