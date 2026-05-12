13d4d7b52976bebd3d3af0851d72e4a53c68c895
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2026-05-12 14:49:15 +1200

    Module 4: Completed Exercise 9 part 1 where we used the Accordion library and added arrow

--- src/module4exercises/Libraries/FAQ/FrequentlyAskedQuestions.js
+++ src/module4exercises/Libraries/FAQ/FrequentlyAskedQuestions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as Accordion from '@radix-ui/react-accordion';
-
+import { ChevronDown } from 'react-feather';
 import styles from './FrequentlyAskedQuestions.module.css';
 
 function FrequentlyAskedQuestions({ data }) {
@@ -10,7 +10,10 @@ function FrequentlyAskedQuestions({ data }) {
       data.map(({id, question, answer}) => (
 		<Accordion.Item className={styles.item} key={id} value={id}>
   			<Accordion.Header>
-  				<Accordion.Trigger className={styles.trigger}>{question}</Accordion.Trigger>
+  				<Accordion.Trigger className={styles.trigger}>
+                    {question}
+                    <ChevronDown />
+                </Accordion.Trigger>
   			</Accordion.Header>
   			<Accordion.Content>{answer}</Accordion.Content>
   		</Accordion.Item>
