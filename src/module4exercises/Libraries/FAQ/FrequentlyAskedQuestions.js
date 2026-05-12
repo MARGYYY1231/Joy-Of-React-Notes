71cf0d01d888c797a91d725852442a5c0f73cfb7
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2026-05-12 14:38:42 +1200

    Module 4: Completed Exercise 9 part 1 where we used the Accordion library

---
+++ src/module4exercises/Libraries/FAQ/FrequentlyAskedQuestions.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as Accordion from '@radix-ui/react-accordion';
+
+import styles from './FrequentlyAskedQuestions.module.css';
+
+function FrequentlyAskedQuestions({ data }) {
+  return (
+    <Accordion.Root type='single' collapsible={true}>
+      {
+      data.map(({id, question, answer}) => (
+		<Accordion.Item className={styles.item} key={id} value={id}>
+  			<Accordion.Header>
+  				<Accordion.Trigger className={styles.trigger}>{question}</Accordion.Trigger>
+  			</Accordion.Header>
+  			<Accordion.Content>{answer}</Accordion.Content>
+  		</Accordion.Item>
+        ))
+      }
+	</Accordion.Root>
+  );
+}
+
+export default FrequentlyAskedQuestions;
